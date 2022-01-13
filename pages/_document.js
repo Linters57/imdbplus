bb2f15f17895a319dd91673ed15cb49208b8f6fe
Lintie420 <linthoutrobbe@gmail.com>
2022-01-13 21:30:06 +0100

    chat

--- pages/_document.js
+++ pages/_document.js
@@ -32,7 +32,7 @@ export default class MyDocument extends Document {
             (function(){
             var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
             s1.async=true;
-            s1.src='https://embed.tawk.to/XXXXXXXXXXXXXXXXXXXXXXXX/XXXXXXXXX';
+            s1.src='https://embed.tawk.to/61e088d0b84f7301d32ae5d1/1fpahcsck';
             s1.charset='UTF-8';
             s1.setAttribute('crossorigin','*');
             s0.parentNode.insertBefore(s1,s0);})();`,
